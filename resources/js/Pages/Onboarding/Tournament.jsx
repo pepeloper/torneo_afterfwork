809011378d5243350073c3a36f8ff9c5ac1e86cb
Pepe <pgarciag93@gmail.com>
2024-02-09 23:35:56 +0100

    wip

--- resources/js/Pages/Onboarding/Tournament.jsx
+++ resources/js/Pages/Onboarding/Tournament.jsx
@@ -1,16 +1,21 @@
 import { Link, Head, useForm } from '@inertiajs/react';
 import ApplicationLogo from "@/Components/ApplicationLogo";
 import CookieConsent from "react-cookie-consent";
-import { Button, ButtonGroup, Tab, TabPanel, Tabs, TabsBody, TabsHeader, Typography } from "@material-tailwind/react";
+import { Button, ButtonGroup, Dialog, Tab, TabPanel, Tabs, TabsBody, TabsHeader, Typography } from "@material-tailwind/react";
 import classNames from "classnames";
 import Footer from "@/Components/Landing/Footer";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import AppAvatar from "@/Components/AppAvatar";
 import FakeGameCard from "./FakeGameCard";
+import Modal from "@/Components/Modal";
+import InputLabel from "@/Components/InputLabel";
+import TextInput from "@/Components/TextInput";
+import InputError from "@/Components/InputError";
 
 export default function Welcome({ players, courts }) {
+  const [registerModal, setRegisterModal] = useState(false);
 
-  const { data, setData, get, processing, errors, reset } = useForm({
+  const { data } = useForm({
     number_of_players: players,
     courts: courts,
   });
@@ -51,11 +56,14 @@ export default function Welcome({ players, courts }) {
     })
   }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (typeof fathom !== "undefined") fathom.trackEvent('onboarding started');
-    get(route('onboarding.tournament', { players: data.number_of_players, courts: data.courts }));
-  }
+  const { data: accountData, setData, post, processing, errors, reset } = useForm({
+    number_of_players: Number(players),
+    courts: Number(courts),
+    user_name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+  });
 
   return (
     <>
@@ -76,12 +84,12 @@ export default function Welcome({ players, courts }) {
           {playersArray.map(u => <AppAvatar key={u.id} user={u} className="border-2 border-white hover:z-10 focus:z-10" />)}
         </section>
         <section className="px-4 mt-5">
-          <div className="flex items-center space-x-3 px-4 py-3 border border-deep-orange-600 rounded-lg bg-deep-orange-50 text-gray-800">
+          <button onClick={() => setRegisterModal(true)} className="flex items-center space-x-3 px-4 py-3 border border-deep-orange-600 rounded-lg bg-deep-orange-50 text-gray-800">
             <svg className="min-w-[24px] min-h-[24px]" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 9V12.75M21 12C21 13.1819 20.7672 14.3522 20.3149 15.4442C19.8626 16.5361 19.1997 17.5282 18.364 18.364C17.5282 19.1997 16.5361 19.8626 15.4442 20.3149C14.3522 20.7672 13.1819 21 12 21C10.8181 21 9.64778 20.7672 8.55585 20.3149C7.46392 19.8626 6.47177 19.1997 5.63604 18.364C4.80031 17.5282 4.13738 16.5361 3.68508 15.4442C3.23279 14.3522 3 13.1819 3 12C3 9.61305 3.94821 7.32387 5.63604 5.63604C7.32387 3.94821 9.61305 3 12 3C14.3869 3 16.6761 3.94821 18.364 5.63604C20.0518 7.32387 21 9.61305 21 12ZM12 15.75H12.008V15.758H12V15.75Z" stroke="#E64A19" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
             <p className="text-sm font-medium leading-5">Para poder añadir jugadores necesitas crear una cuenta. Haz click aquí</p>
-          </div>
+          </button>
         </section>
         <section className="px-4 mt-7">
           <Typography variant="h4" className="text-gray-900">Ranking</Typography>
@@ -121,7 +129,7 @@ export default function Welcome({ players, courts }) {
         <section className="px-4 mt-7 pb-7 relative">
           <Typography variant="h4" className="text-gray-900">Partidos</Typography>
           <div className="mt-4 overflow-hidden">
-            <div className="w-full absolute inset-0 top-32 z-50" style={{ background: "linear-gradient(0deg, rgba(250,250,250,1) 25%, rgba(250,250,250,0.5) 60%, rgba(250,250,250,0) 100%)"}} />
+            <div className="w-full absolute inset-0 top-32 z-50" style={{ background: "linear-gradient(0deg, rgba(250,250,250,1) 25%, rgba(250,250,250,0.5) 60%, rgba(250,250,250,0) 100%)" }} />
             <Tabs value={tournament.groups[0].id}>
               <TabsHeader>
                 {tournament.groups.map(({ name, id }) => (
@@ -163,6 +171,80 @@ export default function Welcome({ players, courts }) {
       >
         Este sitio web utiliza cookies para mejorar la experiencia del usuario.
       </CookieConsent>
+      <Dialog open={registerModal} handler={() => setRegisterModal(false)}>
+        <form className="w-full px-6 py-6 space-y-5">
+          <Typography variant="h4" className="text-gray-900">Crea tu cuenta</Typography>
+
+          <div>
+            <InputLabel htmlFor="name" value="Nombre" />
+
+            <TextInput
+              id="name"
+              name="name"
+              value={accountData.user_name}
+              className="mt-1.5 block w-full"
+              autoComplete="name"
+              onChange={(e) => setData('user_name', e.target.value)}
+              required
+            />
+
+            <InputError message={errors.user_name} className="mt-2" />
+          </div>
+
+          <div>
+            <InputLabel htmlFor="email" value="Correo electrónico" />
+
+            <TextInput
+              id="email"
+              type="email"
+              name="email"
+              value={accountData.email}
+              className="mt-1.5 block w-full"
+              autoComplete="username"
+              onChange={(e) => setData('email', e.target.value)}
+              required
+            />
+
+            <InputError message={errors.email} className="mt-2" />
+          </div>
+
+          <div>
+            <InputLabel htmlFor="password" value="Contraseña" />
+
+            <TextInput
+              id="password"
+              type="password"
+              name="password"
+              value={accountData.password}
+              className="mt-1.5 block w-full"
+              autoComplete="new-password"
+              onChange={(e) => setData('password', e.target.value)}
+              required
+            />
+
+            <InputError message={errors.password} className="mt-2" />
+          </div>
+
+          <div>
+            <InputLabel htmlFor="password_confirmation" value="Repite la contraseña" />
+
+            <TextInput
+              id="password_confirmation"
+              type="password"
+              name="password_confirmation"
+              value={accountData.password_confirmation}
+              className="mt-1.5 block w-full"
+              autoComplete="new-password"
+              onChange={(e) => setData('password_confirmation', e.target.value)}
+              required
+            />
+
+            <InputError message={errors.password_confirmation} className="mt-2" />
+          </div>
+
+          <Button type="submit" variant="gradient" color="light-green" fullWidth>Enviar</Button>
+        </form>
+      </Dialog>
     </>
   );
 }
